1649a882a630fcb27afad96966b6fe76379f0a30
dzikrisyairozi <dikiogres@gmail.com>
2023-11-01 17:46:37 +0700

    feat: implement google maps

--- app/src/main/java/com/example/androidgooglemaps/MainActivity.kt
+++ app/src/main/java/com/example/androidgooglemaps/MainActivity.kt
@@ -1,11 +1,95 @@
-package com.example.androidgooglemaps
+package com.example.androidgooglemaps;
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.app.Activity
+
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.androidgooglemaps.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var map: GoogleMap
+    private lateinit var latitudeInput: EditText
+    private lateinit var longitudeInput: EditText
+    private lateinit var submitButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        latitudeInput = findViewById(R.id.latitudeInput)
+        longitudeInput = findViewById(R.id.longitudeInput)
+        submitButton = findViewById(R.id.submitButton)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync { googleMap ->
+            map = googleMap
+            enableMyLocation()
+        }
+
+        submitButton.setOnClickListener {
+            val latitude = latitudeInput.text.toString().toDoubleOrNull()
+            val longitude = longitudeInput.text.toString().toDoubleOrNull()
+
+            if (latitude != null && longitude != null) {
+                val location = LatLng(latitude, longitude)
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15f))
+            } else {
+                Toast.makeText(this, "Invalid latitude or longitude", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+            map.isMyLocationEnabled = true
+        } else {
+            checkLocationPermission()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    enableMyLocation()
+                } else {
+                    Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+
+    companion object {
+        const val PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1
     }
 }
