82dc1e1896c59e0c407b3f179374a6fdfdf8013c
dzikrisyairozi <dikiogres@gmail.com>
2023-11-02 20:25:54 +0700

    feat: add geocoder function

--- app/src/main/java/com/example/androidgooglemaps/MainActivity.kt
+++ app/src/main/java/com/example/androidgooglemaps/MainActivity.kt
@@ -16,13 +16,18 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import android.location.Address
+import android.location.Geocoder
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var map: GoogleMap
     private lateinit var latitudeInput: EditText
     private lateinit var longitudeInput: EditText
+    private lateinit var locationNameInput: EditText // New EditText
     private lateinit var submitButton: Button
+    private lateinit var submitLocationButton: Button // New Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +35,9 @@ class MainActivity : AppCompatActivity() {
 
         latitudeInput = findViewById(R.id.latitudeInput)
         longitudeInput = findViewById(R.id.longitudeInput)
+        locationNameInput = findViewById(R.id.locationNameInput) // Initialize
         submitButton = findViewById(R.id.submitButton)
+        submitLocationButton = findViewById(R.id.submitLocationButton) // Initialize
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
@@ -51,8 +58,32 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        submitLocationButton.setOnClickListener {
+            val locationName = locationNameInput.text.toString().trim()
+            try {
+                val location = getLocationFromName(locationName)
+                location?.let {
+                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(it, 15f))
+                } ?: Toast.makeText(this, "Location not found", Toast.LENGTH_SHORT).show()
+            } catch (e: IOException) {
+                Toast.makeText(this, "Geocoder service not available", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
         checkLocationPermission()
     }
+
+    private fun getLocationFromName(locationName: String): LatLng? {
+        val geocoder = Geocoder(this)
+        val addresses: List<Address>? = geocoder.getFromLocationName(locationName, 1)
+        if (addresses != null && addresses.isNotEmpty()) {
+            val address = addresses[0]
+            return LatLng(address.latitude, address.longitude)
+        }
+        return null
+    }
+
 
     private fun checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
